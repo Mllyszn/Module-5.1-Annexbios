c8e95b02186772e5a8bc3a77161c5c4e655bb900
Mllyszn <melvinlieuw121107@gmail.com>
2024-09-03 13:56:36 +0200

    Normal Structure

---
+++ assets/php/Homepage.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="/assets/css/HomeStyle.css">
+</head>
+
+<body>
+
+    <?php
+    include ('Header.php')
+        ?>
+
+    <div class="">
+        <P>Leeg</P>
+    </div>
+
+    <?php
+    include ('Footer.php')
+        ?>
+
+</body>
+
+</html>
