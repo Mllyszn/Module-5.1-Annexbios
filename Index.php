29337acc0428a04a7782282890bcd88232dbe04d
Mllyszn <melvinlieuw121107@gmail.com>
2024-09-13 13:49:32 +0200

    udate

--- Index.php
+++ Index.php
@@ -16,15 +16,28 @@
         <img src="/assets/img/AnnexBios_Woerden_logo.png" alt="logo">
         <a href="#" class="header-btn"><i class="fa-solid fa-user"></i>Film agenda</a>
     </div>
+
     <div class="container movie-details">
+        <div class="inleiding">
+            <h3>Welkom bij Annexbios Woerden</h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
+                into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+                release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+                software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
         <div class="row">
             <div class="col-md-6 left-box">
                 <h1>film 1</h1>
-                <p>film description/lorem ipsum</p>
-                <a href="#" class="ticket-bestelling-btn"><i class="npm i bootstrap-icons"></i>Bestel Tickets</a>
-
+                <h3>Welkom bij Annexbios Woerden</h3>
+                <a href="#" class="ticket-bestelling-btn">Bestel Tickets</a>
             </div>
-            <div class="col-md-6"></div>
+            <div class="col-md-6 text-center">
+                <div class="bios-foto">
+                    <img src="/assets/img/046_2160.jpg" class="theater-img" alt="theater">
+                </div>
+            </div>
         </div>
     </div>
 
