df0d1b62c6415e556bc67d1239ee4ac08dd2dd30
demonboy666-1 <94363183+demonboy666-1@users.noreply.github.com>
2024-09-17 15:51:49 +0200

    Update filmAgenda.php

--- filmAgenda.php
+++ filmAgenda.php
@@ -3,22 +3,23 @@
 <h2 class="col-md-6 left-box">Draaiende films</h2>
 
 <?php
-$amount = $totalDbMovies;
+$amount = 10;
 ?>
 
 <?php for ($x = 0;$x <= $amount - 1;$x++): ?>
 <div class="col-4 mb-3">
-   <div class="card w-100">
-      <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/e7c9a8f5-bd80-495a-8f21-d90909c57eb0/d97uzxj-18b79a09-44f0-4531-a322-4947358229ae.png">
+   <div class="card w-25">
+      <img src="<?php echo $allMovies[$x + 1]["banner_path"] ?>" width="158", height="250">
       <div class="card-body">
-         <h5 class="card-title"><?php echo $allMovies[$x + 1]["title"] ?></h5>
-         <p class="card-text">&euro; <?php echo $allMovies[$x + 1]["rating"] ?></p>
-         <a href="filmDetails.php?film=<?php echo $allMovies[$x + 1]["id"] ?>" class="ticket-bestelling-btn">Boeken</a>
+         <h6 class="card-title"><?php echo $allMovies[$x + 1]["title"] ?></h6>
+         <p class="card-text">Rating <?php echo $allMovies[$x + 1]["rating"] ?></p>
+         <p class="card-text"><?php echo $allMovies[$x + 1]["duration"] ?></p>
+         <a href="filmDetails.php?film=<?php echo $allMovies[$x + 1]["id"] ?>" class="col-md-6 left-box">Details</a>
       </div>
    </div>
 </div>
-<?php endfor; ?>  
+<?php endfor;?>  
 
 </div>
 
-<?php include('core/Footer.php'); ?>
+<?php include('core/Footer.php');?>
